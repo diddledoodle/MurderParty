6ac92b0d63cf9c111c894658a78c58166ab97ed8
diddledoodle <samia.jaffar@loop.colum.edu>
2020-05-18 03:34:45 -0500

    Adds final quest, adds main menu, adds portraits, adds credits

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using PixelCrushers.DialogueSystem;
 
 public class SceneLoader : MonoBehaviour
 {
     public void LoadGameScene(string _levelName)
     {
+        DialogueManager.StopConversation();
         SceneManager.LoadSceneAsync(_levelName);
     }
 }
